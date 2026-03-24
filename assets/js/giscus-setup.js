de191f0cfaa44cb0aa39b05df5a4fd5f75eb0052
CrazyGeG <44589264+CrazyGeG@users.noreply.github.com>
2026-03-24 19:31:12 +0000

    Deploying to gh-pages from @ CrazyGeG/An_Yu.github.io@eed664df77e96f5de8261fc17585fe2d8cf0add1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "CrazyGeG/An_Yu",
+    "data-repo": "CrazyGeG/An_Yu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
