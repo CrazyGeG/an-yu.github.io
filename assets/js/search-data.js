6620e812d09d46710c98831210d93951ce6316dc
CrazyGeG <44589264+CrazyGeG@users.noreply.github.com>
2026-03-27 19:42:56 +0000

    Deploying to gh-pages from @ CrazyGeG/an-yu.github.io@340ed0375da5649da3e29fa88076e46994fcff79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/an-yu.github.io/repositories/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/an-yu.github.io/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
